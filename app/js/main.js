13107fb302bb8e8c582ef58f7baa1d80a6a783ff
OlegRkv <oleg-rkv@yandex.ru>
2022-01-15 18:16:48 +0400

    responsive header + hero

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,9 @@ $(function () {
   $('.reviews__slider').slick({
     slidesToShow: 2,
   });
+
+  $('.menu__burger').on('click', function(){
+    $('.menu__burger').toggleClass('menu__burger--active');
+    $('.menu__wrapper').toggleClass('menu__wrapper--active');
+  });
 });
